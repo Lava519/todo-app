f10c7c0901038fb4197b29d2ea752695b624a87e
Lava519 <lavaboi14@gmail.com>
2023-11-05 22:54:22 +0100

    Updated styles

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -14,13 +14,13 @@ export default function AddTask(func) {
 
     return (
         <div className="add-menu">
-            <label className="title-label" htmlFor="title">Title</label>
+            <label className="title-label label" htmlFor="title">Title</label>
             <input onChange={updateTitle} className="input-field" type="text" id="title"></input>
-            <label className="description-label" htmlFor="description">Description</label>
+            <label className="description-label label" htmlFor="description">Description</label>
             <textarea onChange={updateDescription} className="input-field" rows="3" id="description"></textarea>
             <div className="buttons">
-            <button className="menu-cancel-button" onClick={func.toggleShown}>Cancel</button>
-            <button className="menu-add-button" onClick={()=>{func.createTask(title, description)}}>ADD</button>
+            <button className="menu-cancel-button menu-button" onClick={func.toggleShown}>-</button>
+            <button className="menu-add-button menu-button" onClick={()=>{func.createTask(title, description)}}>+</button>
             </div>
         </div>
     )
